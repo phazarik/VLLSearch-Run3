9981ed6f9edbab9c0031e9b27f2d7f20c4d1da6b
phazarik <prachurjyahazarika@gmail.com>
2025-07-07 11:40:28 +0530

    Plot combiner tools

--- StackedPlotMaker/combine_across_campaigns.py
+++ StackedPlotMaker/combine_across_campaigns.py
@@ -5,6 +5,7 @@ from math import ceil, sqrt
 parser = argparse.ArgumentParser()
 parser.add_argument('--plotname', type=str, required=True, help='Plot file name to search for (e.g., "efficiency.png").')
 parser.add_argument('--find',     type=str, required=True, help='Directory to search within.')
+parser.add_argument('--name',     type=str, required=False, help='Output name tag without extension.')
 parser.add_argument('--test',     action='store_true', required=False, help='Run once.')
 parser.add_argument('--dryrun',   action='store_true', required=False, help='Dryrun.')
 args = parser.parse_args()
@@ -12,25 +13,23 @@ args = parser.parse_args()
 search_key = args.find
 plotname   = args.plotname
 
-campaigns = ["2018_UL", "2017_UL", "2016postVFP_UL", "2016preVFP_UL"]
+#campaigns = ["2018_UL", "2017_UL", "2016postVFP_UL", "2016preVFP_UL"]
+campaigns = ["Run3Summer22", "Run3Summer22EE", "Run3Summer23", "Run3Summer23EE"]
 channels  = ["mm", "me", "em", "ee"]
 
-parent_dir = os.path.dirname(search_key.rstrip('/'))
-partial_key = os.path.basename(search_key.rstrip('/'))
-
+parent_dir = search_key.rstrip('/')
 matching_dirs = []
 for subdir in os.listdir(parent_dir):
     full_path = os.path.join(parent_dir, subdir)
-    if not os.path.isdir(full_path) or partial_key not in subdir: continue
-
+    if not os.path.isdir(full_path):                 # remove partial_key check
+        continue
     matched_campaign = next((c for c in campaigns if c in subdir), None)
     matched_channel  = next((ch for ch in channels if subdir.endswith("_" + ch)), None)
-    
     if matched_campaign and matched_channel:
         matching_dirs.append(((matched_campaign, matched_channel), full_path))
 
 if not matching_dirs:
-    print(f"No matching directories found for partial search key: {partial_key}")
+    print(f"\033[31mNo matching directories found for partial search key: {partial_key}\033[0m")
     sys.exit(1)
 
 ordered_dirs = []
@@ -42,7 +41,7 @@ for c in campaigns:
 
 images = []
 count = 0
-print('\nCombining the following images:')
+print('\n==> Combining the following images:')
 for d in ordered_dirs:
     img_path = os.path.join(d, plotname + '.png')
     if os.path.exists(img_path):
@@ -69,6 +68,8 @@ if not args.dryrun and images:
 
     out_dir = os.path.join("plots", "combined")
     os.makedirs(out_dir, exist_ok=True)
-    out_path = os.path.join(out_dir, f"combined_{os.path.basename(search_key.strip('/'))}_{plotname}.png")
+    out_name = f"combined_{os.path.basename(search_key.strip('/'))}_{plotname}.png"
+    if args.name: out_name = f"combined_{args.name}_{plotname}.png"
+    out_path = os.path.join(out_dir, out_name)
     canvas.save(out_path)
-    print(f"\nSaved: {out_path}")
+    print(f"\nSaved: \033[93m{out_path}\033[0m")
